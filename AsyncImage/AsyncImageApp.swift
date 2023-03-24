ec42fa1ef358bdac56a94a65f2efb2ce3e6c1ea6
Rehnuma <rehnuma.reza@stitelnetworks.com>
2023-03-24 10:10:33 +0600

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Rehnuma Reza on 24/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
